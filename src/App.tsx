cf10c1b07fe4496697c63fd8fc7b3aed1673e0bd
juntaba <lovebaseball697@gmail.com>
2020-07-20 23:13:20 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>hello world</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const logoOptions = { alt: "logo", className: "App-logo", src: logo };
+    const title = "こんにちは React";
+    const targets = ["World", "Kanae", "Yukina"];
+    return (
+      <div className="App">
+        <header className="App-header">
+          {" "}
+          {
+            // コメントはこう書く
+          }
+          <img {...logoOptions} />
+          {title && <p>{title}</p>}
+          {targets.map((target) => (
+            <p>Hello, {target}!</p>
+          ))}{" "}
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
